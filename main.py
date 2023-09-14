997cb0706e515d32daaba8a505359faeff46540a
Haseenabegum28 <144517410+Haseenabegum28@users.noreply.github.com>
2023-09-14 14:31:43 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-num=int(input("please Enter the Number you wish:"))
-if (num%4==0):
-  if(num%100==0):
-    if(num%400==0):
-      print("%d is a leap year"%num)
-    else:
-      print("%d is not"%num)
-  else:
-    print("%d is a leap year"%num)
-else:
-  print("%d is not"%num)
+class bankaccount:
+  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+   self.__account_number = account_number
+   self.__account_holder_name=account_holder_name
+   self.__account_balance = initial_balance
+  def deposit (self, amount ):
+     if amount >0:
+      self.__account_balance += amount
+      print("deposited ₹{}. new balance : ₹{}". format(amount,self.__account_balance))
+     else:
+       print("invalid deposit amount. please deposit a positive amount.")
+  def withdraw(self, amount ):
+     if amount > 0 and amount  <= self.__account_balance:
+       self.__account_balance -= amount
+       print("withdraw ₹{}. new balance: ₹{}" .format(amount,self.__account_balance))
+     else:
+        print("invalid withdrawal amount or insufficient balance.")
+  def display_balance(self):
+      print("account balance for{} (account #{}): ₹{}" . format(
+         self.__account_holder_name, self .__account_number,
+        self.__account_balance))
+account = bankaccount(account_number="123456789", account_holder_name="hari prabhu", initial_balance=5000.0)
+account.display_balance()
+account.deposit(500.0)
+account.withdraw(200.0
